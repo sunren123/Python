451483775dc7f8946a99f8e8ea4f038e78276823
sunren <462227979@qq.com>
2018-11-15 20:21:30 +0800

    add tcp server

--- socket/客户端.py
+++ socket/客户端.py
@@ -4,10 +4,22 @@ import socket
 
 
 def main():
-    tcp_socket= socket.socket(socket.AD_INET, socket.SOCK_STREAM)
+    tcp_server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    tcp_socket.connect()
+    tcp_server_socket.bind(("",7890))
 
+    tcp_server_socket.listen(128)
+
+    print("-----1------")
+
+    new_client_socket,client_addr =tcp_server_socket.accept()
+    print(client_addr)
+
+    recv_data =new_client_socket.recv(1024)
+    new_client_socket.send("回显消息".encode("utf-8"))
+
+    new_client_socket.close()
+    tcp_server_socket.close()
 
 if __name__ == '__main__':
     main()
